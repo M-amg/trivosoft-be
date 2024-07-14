744573e48a174f889bb0c27173a0a76dc7138a4e
amghar <amghar@amg.com>
2024-07-14 23:10:41 +0100

    first commit

---
+++ src/test/java/com/trivosoft/caravan/CaravanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trivosoft.caravan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaravanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
